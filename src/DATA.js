47da2cdb0fb42b6acd5809216ba5abcf8d7e38cb
voldemar <vladimir.gulko@ukr.net>
2020-01-17 12:22:47 +0200

    first|

---
+++ src/DATA.js
@@ -0,0 +1,39 @@
+const PRODUCTS = [
+	{
+		id: 1,
+		name: "Apple",
+		description: "CCO (Red apples)",
+		img: "https://tachyons.io/img/avatar_1.jpg",
+		price: 100
+	},
+	{
+		id: 2,
+		name: "A Cat",
+		description: "CCO (Red apples)",
+		img: "https://tachyons.io/img/avatar_1.jpg",
+		price: 100
+	},
+	{
+		id: 3,
+		name: "Milk",
+		description: "Milk for the lactose tollerent",
+		img: "https://tachyons.io/img/avatar_1.jpg",
+		price: 100
+	},
+	{
+		id: 4,
+		name: "Banana",
+		description: "i need those carbs",
+		img: "https://tachyons.io/img/avatar_1.jpg",
+		price: 100
+	},
+	{
+		id: 5,
+		name: "Peanuts",
+		description: "Very delicious",
+		img: "https://tachyons.io/img/avatar_1.jpg",
+		price: 100
+	}
+];
+
+export default PRODUCTS;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {render} from 'react-dom';
+import './style.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import reducers from "./redux/reducers"
+import App from "./containers/App_container";
+
+const Store = createStore(reducers);
+
+render(
+	<Provider store={Store}><App /></Provider>, 
+	document.getElementById('root')
+);
